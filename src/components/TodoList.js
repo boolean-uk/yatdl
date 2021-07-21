3736832a1f0d6b124532f5b94ff78d46c9cb51e4
dearshrewdwit <dearshrewdwit@gmail.com>
2021-07-21 13:09:42 +0100

    use react to store input value, not html

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,14 +4,17 @@ import Todo from './Todo'
 class TodoList extends Component {
   constructor(props) {
     super(props)
-    this.state = { todos: [] }
+    this.state = { todos: [], text: '' }
   }
 
   addTodo = () => {
-    const todo = document.getElementById("todoList-textbox").value
     const newTodos = this.state.todos
-    newTodos.push(todo)
+    newTodos.push(this.state.text)
     this.setState({todos: newTodos})
+  }
+
+  handleChange = (event) => {
+    this.setState({text: event.target.value})
   }
 
   clear = () => {
@@ -22,7 +25,7 @@ class TodoList extends Component {
     return (
       <div>
         <h1 id="todoList-heading">Yet Another Todo List!</h1>
-        <input id="todoList-textbox" type="text"></input>
+        <input id="todoList-textbox" type="text" onChange={this.handleChange}></input>
         <button id="todoList-button" onClick={this.addTodo}>Save</button>
         <a id="todoList-clearLink" href="#" onClick={this.clear}>clear</a>
         {this.state.todos.map((todo, index) => <Todo id={index} key={index} text={todo} />)}
